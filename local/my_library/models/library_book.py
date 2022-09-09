70b9025f5f62426d59a9101ff522d7d00016ca44
NoeBerdoz <berdoznoe@gmail.com>
2022-09-09 09:34:10 +0200

    Changed library records display name

--- local/my_library/models/library_book.py
+++ local/my_library/models/library_book.py
@@ -3,9 +3,19 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'  # This will be library_book in the database
-    name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many(
-        'res.partner',
-        string='Authors'
-    )
+    _description = 'Library Book'  # Model user-friendly title
+    _order = 'date_release desc, name'  # Sort the records from the newest to the oldest, then by title
+    _rec_name = 'short_name'  # Use short_name as record representation
+    short_name = fields.Char('Short Title', required=True)  # Short Book title
+    name = fields.Char('Title', required=True)  # Book title
+    date_release = fields.Date('Release Date')  # Book release date
+    author_ids = fields.Many2many('res.partner', string='Authors')  # Book authors, authors are partners, N to N
+
+    # Override name_get function to set display_name as name with date_release
+    def name_get(self):
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+
+        return result
