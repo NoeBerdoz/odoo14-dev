f9a84690ea3ec58a6d07570ad10e68243bbec9f4
NoeBerdoz <berdoznoe@gmail.com>
2022-09-09 09:33:32 +0200

    Module base security configuration

--- local/my_library/__manifest__.py
+++ local/my_library/__manifest__.py
@@ -7,6 +7,6 @@
     'category': 'Uncategorized',
     'version': '14.0.1.0.0',
     'depends': ['base'],
-    'data':['views/library_book.xml'],
+    'data':['views/library_book.xml', 'security/groups.xml', 'security/ir.model.access.csv'],
     'demo':['demo.xml']
  }
