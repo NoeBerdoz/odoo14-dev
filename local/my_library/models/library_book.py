91de05b6740bc94a65eb6668b0912224028f53d4
NoeBerdoz <berdoznoe@gmail.com>
2022-09-09 10:19:36 +0200

    Added new Book fields

--- local/my_library/models/library_book.py
+++ local/my_library/models/library_book.py
@@ -9,7 +9,21 @@ class LibraryBook(models.Model):
     short_name = fields.Char('Short Title', required=True)  # Short Book title
     name = fields.Char('Title', required=True)  # Book title
     date_release = fields.Date('Release Date')  # Book release date
+    date_updated = fields.Datetime('Last Updated')  # Book time updated
     author_ids = fields.Many2many('res.partner', string='Authors')  # Book authors, authors are partners, N to N
+    notes = fields.Text('Internal Notes')  # Book notes
+    state = fields.Selection(
+        [
+            ('draft', 'Not Availabke'),
+            ('available', 'Available'),
+            ('lost', 'Lost')
+        ],
+        'State')  # Book state options
+    description = fields.Html('Description')  # Description, but why HTML ?
+    cover = fields.Binary('Book Cover')  # Binary stand for a file stored
+    out_of_print = fields.Boolean('Out of Print?')  # Book print status
+    pages = fields.Integer('Number of Pages')  # Book page numbers
+    reader_rating = fields.Float('Reader Average Rating', digits=(14, 4),)  # Rating, digits prm is decimal precision
 
     # Override name_get function to set display_name as name with date_release
     def name_get(self):
