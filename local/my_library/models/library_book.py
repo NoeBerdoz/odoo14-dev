a51dc2d7ed891532aa1dfb11ee1e22cc6be9620d
NoeBerdoz <berdoznoe@gmail.com>
2022-09-09 12:05:35 +0200

    Model pages field attributes

--- local/my_library/models/library_book.py
+++ local/my_library/models/library_book.py
@@ -6,7 +6,7 @@ class LibraryBook(models.Model):
     _description = 'Library Book'  # Model user-friendly title
     _order = 'date_release desc, name'  # Sort the records from the newest to the oldest, then by title
     _rec_name = 'short_name'  # Use short_name as record representation
-    short_name = fields.Char('Short Title', required=True)  # Short Book title
+    short_name = fields.Char('Short Title', translate=True, index=True, required=True)  # Short Book title
     name = fields.Char('Title', required=True)  # Book title
     date_release = fields.Date('Release Date')  # Book release date
     date_updated = fields.Datetime('Last Updated')  # Book time updated
@@ -14,15 +14,23 @@ class LibraryBook(models.Model):
     notes = fields.Text('Internal Notes')  # Book notes
     state = fields.Selection(
         [
-            ('draft', 'Not Availabke'),
+            ('draft', 'Not Available'),
             ('available', 'Available'),
             ('lost', 'Lost')
         ],
-        'State')  # Book state options
-    description = fields.Html('Description')  # Description, but why HTML ?
+        'State', default="draft")  # Book state options
+    # Description with HTML type field for user edition
+    description = fields.Html('Description', sanitize=True, strip_style=False)
     cover = fields.Binary('Book Cover')  # Binary stand for a file stored
     out_of_print = fields.Boolean('Out of Print?')  # Book print status
-    pages = fields.Integer('Number of Pages')  # Book page numbers
+    # Book page numbers
+    pages = fields.Integer(
+        'Number of Pages',
+        groups='base.group_user',
+        states={'lost': [('readonly', True)]},
+        help='Total book page count',
+        company_dependant=False
+    )
     reader_rating = fields.Float('Reader Average Rating', digits=(14, 4),)  # Rating, digits prm is decimal precision
 
     # Override name_get function to set display_name as name with date_release
